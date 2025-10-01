63cb9a405c3f58c90466509838ce2059119f5f3f
maarigonzalezz <ma.gonzalez@estudiantec.cr>
2025-09-30 21:56:15 -0600

    avance cartitas

--- VGA_Controller/rectgen.sv
+++ VGA_Controller/rectgen.sv
@@ -1,8 +1,11 @@
-// Modulo para la creacion del rectangulo de test
+// Modulo para la creacion del rectangulo con circulo adentro
 
 module rectgen(
-    input logic [9:0] x, y, left, right, top, bot,
+    input logic [9:0] x, y, 
+    input logic [9:0] left, right, top, bot, // cuadrado
     output logic inrect
 );
+
+    // Lógica del rectángulo
     assign inrect = (x >= left & x < right & y >= top & y < bot);
 endmodule
--- VGA_Controller/videoGen.sv
+++ VGA_Controller/videoGen.sv
@@ -3,16 +3,73 @@
 module videoGen(input logic [9:0] x, y, 
 					output logic [7:0] r, g, b);
 
-logic pixel, rect;
+logic pixel, cA1, cA2, cB1, cB2;
+logic cC1, cC2, cD1, cD2, cE1, cE2;
+logic cF1, cF2, cG1, cG2, cH1, cH2;
 logic [10:0] topS = 10'd360;
 logic [10:0] botS = 10'd411;
 logic [10:0] leftS = 10'd76;
 logic [10:0] rightS = 10'd126;
 
-rectgen rectgen1(x, y, 10'd20, 10'd279, 10'd76, 10'd336, rect);
+// las cartas en la pantalla se veran como una matriz 4x4, por lo cual usaremos variables
+// para alturas anchos y posiciones
+logic [9:0] topF1 = 10'd70;
+logic [9:0] bottomF1 = 10'd140;
 
-// Fondo negro
-    assign r = (rect) ? 8'hFF : 8'hFF; // rectángulo y fondo con rojo
-	assign g = (rect) ? 8'h00 : 8'hFF; // dentro rectángulo = 0 (rojo puro), fuera = verde encendido
-	assign b = (rect) ? 8'h00 : 8'hFF; // dentro rectángulo = 0 (rojo puro), fuera = azul encendido
+logic [9:0] topF2 = 10'd160;
+logic [9:0] bottomF2 = 10'd230;
+
+logic [9:0] topF3 = 10'd250;
+logic [9:0] bottomF3 = 10'd320;
+
+logic [9:0] topF4 = 10'd340;
+logic [9:0] bottomF4 = 10'd410;
+
+logic [9:0] leftC1 = 10'd160;
+logic [9:0] rightC1 = 10'd210;
+
+logic [9:0] leftC2 = 10'd250;
+logic [9:0] rightC2 = 10'd300;
+
+logic [9:0] leftC3 = 10'd340;
+logic [9:0] rightC3 = 10'd390;
+
+logic [9:0] leftC4 = 10'd430;
+logic [9:0] rightC4 = 10'd480;
+
+
+// Cartas: coordenadas
+    carta cartaA1(
+        .x(x), .y(y), 
+        .left(leftC1), .right(rightC1), 
+        .top(topF1), .bot(bottomF1), 
+        .incard(cA1)
+    );
+	 
+	 carta cartaA2(
+        .x(x), .y(y), 
+        .left(leftC2), .right(rightC2), 
+        .top(topF1), .bot(bottomF1), 
+        .incard(cA2)
+    );
+	 
+
+
+// Unión de ambos
+assign cartas_total = cA1 | cA2;
+
+always_comb begin
+    // Fondo negro
+    r = 8'h00;
+    g = 8'h00;
+    b = 8'h00;
+
+    // Si está en cualquiera de los dos rectángulos
+    if (cartas_total) begin
+        r = 8'hFF;   // Rojo
+        g = 8'h00;
+        b = 8'hFF;
+    end
+end
+	
 endmodule
